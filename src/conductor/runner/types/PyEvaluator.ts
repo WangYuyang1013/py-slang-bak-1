cac7f3afcf9baceccfb813a727d83de0aa664478
Wangdahai <wangdahai@192.168.1.137>
2025-03-30 00:36:37 +0800

    v0

--- src/conductor/runner/types/PyEvaluator.ts
+++ src/conductor/runner/types/PyEvaluator.ts
@@ -29,7 +29,7 @@ export class PyEvaluator extends BasicEvaluator {
                 this.options
             );
             //this.conductor.sendOutput(`Result: ${result.representation.toString(result.value)}`);
-            this.conductor.sendOutput(`Result: ${typeof result}`);
+            this.conductor.sendOutput(`Result: ${result}`);
         } catch (error) {
             this.conductor.sendOutput(`Error: ${error instanceof Error ? error.message : error}`);
         }
